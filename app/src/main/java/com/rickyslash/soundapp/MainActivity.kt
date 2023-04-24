77703880280188d8b90a417140df96eea6313e4f
ricky-kiva <ricky.kiva@gmail.com>
2023-04-25 02:31:42 +0700

    add summary

--- app/src/main/java/com/rickyslash/soundapp/MainActivity.kt
+++ app/src/main/java/com/rickyslash/soundapp/MainActivity.kt
@@ -11,4 +11,14 @@ class MainActivity : AppCompatActivity() {
 }
 
 // Some of the Media that is important in Android:
-// - 
+// - SoundPool: short sound effect
+// - MediaPlayer: play audio/video on an app
+// - ExoPlayer: advanced MediaPlayer with Dynamic Adaptive Streaming over HTTP (DASH), smooth streaming, to common encryption
+// - CameraX: make custom camera in an app, including adding effects
+// - Intent Camera: implicit intent to open Android's default camera
+// - Intent Gallery: implicit intent to open Android's default gallery
+
+// audio files are placed on `res/raw`
+// SoundPool saves audio data in memory without compression. suitable to play short audio. the audio file need to be loaded first
+// MediaPlayer suitable for media more than 1 MB. it could also run video & supports playback control. this could also play media from internet
+// ExoPlayer is an opensource library from MediaCodec API. it's more advanced than MediaPlayer
