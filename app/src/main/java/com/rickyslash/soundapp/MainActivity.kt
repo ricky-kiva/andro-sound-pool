772884e9623c49dfc3d23af62c0825920b203236
ricky-kiva <ricky.kiva@gmail.com>
2023-04-25 02:24:33 +0700

    Initial commit

---
+++ app/src/main/java/com/rickyslash/soundapp/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.rickyslash.soundapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
+
+// Some of the Media that is important in Android:
+// - 
