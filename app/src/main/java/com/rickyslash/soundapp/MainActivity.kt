f74b8a23830697b6a7fedd5aeaf23a4752738fd3
ricky-kiva <ricky.kiva@gmail.com>
2023-04-28 16:01:39 +0700

    add SoundPool

--- app/src/main/java/com/rickyslash/soundapp/MainActivity.kt
+++ app/src/main/java/com/rickyslash/soundapp/MainActivity.kt
@@ -1,12 +1,45 @@
 package com.rickyslash.soundapp
 
+import android.media.SoundPool
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var sp: SoundPool
+    private var soundId: Int = 0
+    private var spLoaded = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnSound = findViewById<Button>(R.id.btn_sound_pool)
+
+        sp = SoundPool.Builder() // instantiate SoundPool
+            .setMaxStreams(2) // make the max sound play to 2
+            .build() // build SoundPool
+
+        sp.setOnLoadCompleteListener { _, _, status ->
+            if (status == 0) {
+                // set flags to check if the sound is loaded
+                spLoaded = true
+            } else {
+                Toast.makeText(this@MainActivity, "Fail to load", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        // load the desired sound
+        soundId = sp.load(this, R.raw.clinking_glasses, 1)
+
+        btnSound.setOnClickListener {
+            if (spLoaded) {
+                // play the SoundPool on button click
+                sp.play(soundId, 1f, 1f, 0, 0, 1f)
+            }
+        }
     }
 }
 
